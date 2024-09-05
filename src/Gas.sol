e053ffa5bf188f8e5b36616f90dbf0b42e167ee7
Stanislaw Malinowski <stanislawmalinowski09@gmail.com>
2024-09-05 18:40:40 +0100

    some good steps forward

--- src/Gas.sol
+++ src/Gas.sol
@@ -10,29 +10,35 @@ contract Constants {
 }
 
 contract GasContract is Ownable, Constants {
+    // todo move stuff into notContants contract like above
     uint256 public totalSupply = 0; // cannot be updated
     uint256 public paymentCounter = 0;
     mapping(address => uint256) public balances;
-    uint256 public tradePercent = 12;
+    
+    // todo maybe even less
+    uint64 public tradePercent = 12;
     address public contractOwner;
-    uint256 public tradeMode = 0;
+
+    // DID changed to bool
+    bool public tradeMode = false;
     mapping(address => Payment[]) public payments;
     mapping(address => uint256) public whitelist;
     address[5] public administrators;
     bool public isReady = false;
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-    PaymentType constant defaultPayment = PaymentType.Unknown;
+    // enum PaymentType {
+    //     Unknown,
+    //     BasicPayment,
+    //     Refund,
+    //     Dividend,
+    //     GroupPayment
+    // }
+    // PaymentType constant defaultPayment = PaymentType.Unknown;
 
+    // todo change this to a mapping
     History[] public paymentHistory; // when a payment was updated
 
     struct Payment {
-        PaymentType paymentType;
+        // PaymentType paymentType;
         uint256 paymentID;
         bool adminUpdated;
         string recipientName; // max 8 characters
@@ -78,24 +84,6 @@ contract GasContract is Ownable, Constants {
         }
     }
 
-    modifier checkIfWhiteListed(address sender) {
-        address senderOfTx = msg.sender;
-        require(
-            senderOfTx == sender,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the originator of the transaction was not the sender"
-        );
-        uint256 usersTier = whitelist[senderOfTx];
-        require(
-            usersTier > 0,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
-        );
-        require(
-            usersTier < 4,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user's tier is incorrect, it cannot be over 4 as the only tier we have are: 1, 2, 3; therfore 4 is an invalid tier for the whitlist of this contract. make sure whitlist tiers were set correctly"
-        );
-        _;
-    }
-
     event supplyChanged(address indexed, uint256 indexed);
     event Transfer(address recipient, uint256 amount);
     event PaymentUpdated(
@@ -136,13 +124,12 @@ contract GasContract is Ownable, Constants {
     }
 
     function checkForAdmin(address _user) public view returns (bool admin_) {
-        bool admin = false;
         for (uint256 ii = 0; ii < administrators.length; ii++) {
             if (administrators[ii] == _user) {
-                admin = true;
+                return true;
             }
         }
-        return admin;
+        return false;
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
@@ -209,7 +196,7 @@ contract GasContract is Ownable, Constants {
         Payment memory payment;
         payment.admin = address(0);
         payment.adminUpdated = false;
-        payment.paymentType = PaymentType.BasicPayment;
+        // payment.paymentType = PaymentType.BasicPayment;
         payment.recipient = _recipient;
         payment.amount = _amount;
         payment.recipientName = _name;
@@ -225,8 +212,8 @@ contract GasContract is Ownable, Constants {
     function updatePayment(
         address _user,
         uint256 _ID,
-        uint256 _amount,
-        PaymentType _type
+        uint256 _amount
+        // PaymentType _type
     ) public onlyAdminOrOwner {
         require(
             _ID > 0,
@@ -247,7 +234,7 @@ contract GasContract is Ownable, Constants {
             if (payments[_user][ii].paymentID == _ID) {
                 payments[_user][ii].adminUpdated = true;
                 payments[_user][ii].admin = _user;
-                payments[_user][ii].paymentType = _type;
+                // payments[_user][ii].paymentType = _type;
                 payments[_user][ii].amount = _amount;
                 bool tradingMode = getTradingMode();
                 addHistory(_user, tradingMode);
@@ -257,6 +244,7 @@ contract GasContract is Ownable, Constants {
                     _amount,
                     payments[_user][ii].recipientName
                 );
+                break;
             }
         }
     }
@@ -271,22 +259,16 @@ contract GasContract is Ownable, Constants {
         );
         whitelist[_userAddrs] = _tier;
         if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
             whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
         } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
             whitelist[_userAddrs] = 2;
         }
-        uint256 wasLastAddedOdd = wasLastOdd;
-        if (wasLastAddedOdd == 1) {
+        if (wasLastOdd == 1) {
             wasLastOdd = 0;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
-        } else if (wasLastAddedOdd == 0) {
+            isOddWhitelistUser[_userAddrs] = wasLastOdd;
+        } else if (wasLastOdd == 0) {
             wasLastOdd = 1;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
+            isOddWhitelistUser[_userAddrs] = wasLastOdd;
         } else {
             revert("Contract hacked, imposible, call help");
         }
@@ -296,7 +278,7 @@ contract GasContract is Ownable, Constants {
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public checkIfWhiteListed(msg.sender) {
+    ) public {
         address senderOfTx = msg.sender;
         whiteListStruct[senderOfTx] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
         
---
+++ src/todo.md
@@ -0,0 +1,7 @@
+
+- [ ] function calls order expensive and cheap
+- [ ] break out of loops
+- [ ] move tradepercent into constants?
+- [ ] use bytes[lownumber] for all the stuff
+
+
